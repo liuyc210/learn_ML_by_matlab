2d0f23950e6f0ae8762717a0823ea5fbf52b10e4
unknown <liuyc@MSH-PC2680.marvell.com>
2015-01-12 19:00:41 +0800

    用ANN定义重写

--- perceptor_1p0.m
+++ perceptor_1p0.m
@@ -1,23 +1,23 @@
-n = 1e3;
+n = 1e2;
 
 % generate (x,y)
 RawData = 1*(unidrnd(2,1,n)-1); %between 0 and 1
-x = 2*(RawData -0.5) + normrnd(0,0.6, 1, n);
+x = 2*(RawData -0.5) + normrnd(0,0.8, 1, n);
 RawData(1:10)  
 % figure;plot(x,'.');  
 
 % training perceptor
 
 NumIter = 100;
-theta = ones(1,NumIter); theta(1) = 1;
+theta = ones(1,NumIter); theta(1) = 11;
 mu = 0.05;
 gradient = zeros(1,NumIter);
 h = zeros(1,NumIter);
-for nIter = 2:NumIter-1
+for nIter = 2:NumIter
     for i=2:n
         h(i) = sigmoid(theta(nIter)*x(i));
-        %     gradient(i) = RawData(i)*(1-h(i)) - (1-RawData(i))*h(i);
-        gradient(nIter) = gradient(nIter) + (RawData(i) - h(i))*x(i);  %be care of the gradient
+%         gradient(nIter) = gradient(nIter) + (RawData(i) - h(i))*x(i);  %be care of the gradient
+        gradient(nIter) = gradient(nIter) + (RawData(i) - h(i))*x(i)*h(i)*(1-h(i));  %be care of the gradient
     end
     theta(nIter+1) = theta(nIter) + mu*gradient(nIter);
 end
@@ -27,6 +27,17 @@ subplot(2,2,1); plot(theta,'.'); grid on; title('theta');
 subplot(2,2,2); plot(gradient,'.'); grid on; title('gradient');
 subplot(2,2,3); plot(h,'.'); grid on; title('h');
 subplot(2,2,4); plot(x,'.'); grid on; title('x');
+
+nJ = 300;
+J_theta = zeros(1,nJ);
+for nTheta = 1:nJ
+    for i=1:n
+        J_theta(nTheta) = J_theta(nTheta) + (sigmoid((nTheta-nJ/2)/5*x(i)) - RawData(i))^2;
+    end;
+end
+figure; plot(((1:nJ)-nJ/2)/5, J_theta); grid on; xlabel('theta'); ylabel('cost function');
+
+
 
 
 
--- perceptor_2p0.m
+++ perceptor_2p0.m
@@ -1,36 +1,39 @@
 clear all;
-n = 1e4;
+n = 1e2;
 
 % generate (x,y)
-offset = 1.5;
+offset = 4.5;
 RawData = (unidrnd(2,1,n)-1); %between 0 and 1
-% RawData = [ones(1,n/2), zeros(1,n/2)];
-x = 2.5*(RawData-0.5) + normrnd(0,.0, 1, n) + offset;
+ RawData = [ones(1,n/2), zeros(1,n/2)];
+ RawData = repmat([0,1], 1, n/2);
+x = 0.5*(RawData-0.5) + normrnd(0,.0, 1, n) + offset;
 
 % figure;plot(x,'.');
 
 % training perceptor
-theta0 = zeros(1,n); 
-theta1 = ones(1,n)*1;
-mu0 = 0.01;
-mu1 = 0.03;
-gradient0 = zeros(1,n);
-gradient0 = zeros(1,n);
-h = zeros(1,n);
-for i=2:n
-    h(i) = sigmoid(theta0(i) + theta1(i)*x(i));
-%     gradient(i) = RawData(i)*(1-h(i)) - (1-RawData(i))*h(i);
-    gradient0(i) = (RawData(i) - h(i))*1;  %be care of the gradient
-    gradient1(i) = (RawData(i) - h(i))*x(i);  %be care of the gradient
-    gradient0(i) = (RawData(i)-h(i))*h(i)*(1-h(i))*1;
-    gradient1(i) = (RawData(i)-h(i))*h(i)*(1-h(i))*x(i);
+NumIter = 1000;
 
-        theta0(i+1) = theta0(i) + mu0*gradient0(i);
-        theta1(i+1) = theta1(i) + mu1*gradient1(i);
-%     end
+theta0 = zeros(1,NumIter); 
+theta1 = ones(1,NumIter)*1;
+mu0 = 0.05;
+mu1 = 0.05;
+gradient0 = zeros(1,NumIter);
+gradient1 = zeros(1,NumIter);
+h = zeros(1,n);
+for nIter = 2:NumIter
+    for i=2:n
+        h(i) = sigmoid(theta0(nIter) + theta1(nIter)*x(i));
+        %     gradient(i) = RawData(i)*(1-h(i)) - (1-RawData(i))*h(i);
+        gradient0(nIter) = gradient0(nIter) + (RawData(i) - h(i))*1;  %be care of the gradient
+        gradient1(nIter) = gradient1(nIter) + (RawData(i) - h(i))*x(i);  %be care of the gradient
+%         gradient0(nIter) = gradient0(nIter) + (RawData(i)-h(i))*h(i)*(1-h(i))*1;
+%         gradient1(nIter) = gradient1(nIter) + (RawData(i)-h(i))*h(i)*(1-h(i))*x(i);
+    end
+    theta0(nIter+1) = theta0(nIter) + mu0*gradient0(nIter);
+    theta1(nIter+1) = theta1(nIter) + mu1*gradient1(nIter);
 end
 
-[theta0(n), theta1(n)]
+[theta0(NumIter), theta1(NumIter)]
 figure;
 subplot(2,2,1); plot(theta0, '.'); hold on; plot(theta1,'r.'); grid on; title('theta');
 subplot(2,2,2); plot(gradient0,'.'); hold on; plot(gradient1,'r.'); grid on; title('gradient');
@@ -39,7 +42,7 @@ subplot(2,2,4); plot(x,'.'); grid on; title('x');
 
 x_axis = -10:0.1:10;
 H = zeros(1,length(x_axis));
-for i=1:length(x_axis) H(i) = sigmoid(theta0(n)+theta1(n)*x_axis(i)); end;
+for i=1:length(x_axis) H(i) = sigmoid(theta0(NumIter)+theta1(NumIter)*x_axis(i)); end;
 figure; plot(x_axis, H); grid on; 
 
 
